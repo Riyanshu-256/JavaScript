39fbb00a51de0a4d3500657b91c303e3ab4c79a0
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-01 11:26:51 +0530

    script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -29,15 +29,15 @@ console.log(myFirstJob);
 console.log(myCurrentJob);
 console.log(job1);
 console.log(job2);
-*/
 
 let javaScriptIsFun= true;
 console.log(javaScriptIsFun);
 
-/* console.log(typeof true); */
+ console.log(typeof true); 
 console.log(typeof javaScriptIsFun);
-/*console.log(typeof 37);
-console.log(typeof 'Riyanshu');*/
+console.log(typeof 37);
+console.log(typeof 'Riyanshu');
+
 
 javaScriptIsFun = 'YES!';
 console.log(typeof javaScriptIsFun);
@@ -51,3 +51,44 @@ console.log(typeof year);
 
 console.log(typeof null);
 
+
+let age = 22;
+age = 23;
+
+const birthYear = 1991;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Sharma';
+console.log(lastName);
+*/
+
+//math operator
+const ageRiyanshu = now-2005;
+const ageAditya = now-2001;
+const ageIndu = now-1997;
+console.log(ageRiyanshu, ageAditya, ageIndu);
+
+console.log(ageRiyanshu*2, ageAditya/5, ageIndu%2);
+
+const firstName = 'Riyanshu';
+const lastName = 'Sharma';
+console.log(firstName + ' ' + lastName);
+
+// assignment operator
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+// comparison opeartors
+console.log(ageRiyanshu > ageAditya); // >, <, >=,<=
+console.age(ageIndu >= 18);
+
+const isFullAge = ageIndu >=18;
+
+console.log(now - 2005 > now - 2001 > now - 1997);
