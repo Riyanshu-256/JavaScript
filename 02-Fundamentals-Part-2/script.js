2943c47e35b432a3294fe155d61bedc3b9e6fa71
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-01 22:23:43 +0530

    script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -82,7 +82,7 @@ function fruitProcessor(apples, oranges) {
 }
 console.log(fruitProcessor(2,3));
 */
-
+/* 6.
 //REVIEWING FUNCTIONS
 const calcAge = function(birthYear) {
     return 2025 - birthYear;
@@ -102,3 +102,34 @@ const yearsUntilRetirement = function(birthYear, firstName) {
 }
 yearsUntilRetirement(2005, 'Riyanshu');
 yearsUntilRetirement(2000, 'Anshu');
+*/
+
+//Coding Challenge #1
+
+//Test1
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+// Test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(576, 111);
+
+// Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
