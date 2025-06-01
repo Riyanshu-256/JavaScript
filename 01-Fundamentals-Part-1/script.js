cb8e5c625e26427343c928e9f06ee887daf6bf99
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-01 10:25:03 +0530

    script.js

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,30 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("BARIS");
+console.log(13);
+
+let firstName = "BARIS";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let riyanshu_sharma = "RS";
+let $function = 37;
+
+let Person = 'Riyanshu'
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(riyanshu_sharma);
+console.log(PI);
+console.log(myFirstJob);
+console.log(myCurrentJob);
+console.log(job1);
+console.log(job2);
