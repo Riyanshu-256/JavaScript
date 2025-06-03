dd0ff89ed6ae78f3e8b501d8d795b3568a8b236a
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-03 15:20:30 +0530

    script.js

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -41,6 +41,8 @@ calcAge(2005);
 // printAge();
 */
 
+
+/*
 // Hoisting and TDZ in Practice
 
 // Variables
@@ -84,3 +86,37 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+//console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2025 - birthYear);
+  //console.log(this)
+};
+calcAge(2005);
+
+const calcAgeArrow = birthYear => {
+  console.log(2025 - birthYear);
+  console.log(this)
+};
+calcAge(2005);
+
+const Riyanshu = {
+  year: 2025,
+  calcAge: function (){
+    console.log(this);
+    console.log(2025 - this.year);
+  },
+};
+Riyanshu.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = Riyanshu.calcAge;
+matilda.calcAge();
+
+const f = Riyanshu.calcAge;
+f();
