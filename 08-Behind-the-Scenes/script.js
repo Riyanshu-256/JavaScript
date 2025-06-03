c6b8e7a3f512d8ed31e7c8d9142cfcb284e2b155
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-03 09:41:11 +0530

    script.js

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,16 +1,40 @@
+// SCOPING IN PRACTICE
+
 'use strict';
 
 function calcAge(birthYear) {
-    const age = 2025 - birthYear ;
-   
-    function printAge(){
-        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
-        console.log(output);
-    }
-    printAge();
+  const age = 2025 - birthYear;
 
-    return age;
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1999 && birthYear <= 2007) {
+      var millionaire = true;
+      // Creating new variable with same name as outer scope's variable
+      const firstName = 'Riyanshu';
+
+      // Reasssigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh my god!!!,  you're a millionaire, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millionaire);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
 }
 
 const firstName = 'Riyanshu';
 calcAge(2005);
+// console.log(age);
+// printAge();
