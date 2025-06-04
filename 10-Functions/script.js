ed69941ff0b903488aafc38f9f06096bf1e8f4de
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-04 16:50:41 +0530

    script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/*
 // DEFAULT PARAMETERS
 
 const bookings = [];
@@ -29,10 +28,10 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
-*/
+
 
 //-----------------------------------------------------------------------------------------------------------------------------//
-/*
+
 // HOW PASSING ARGUMENTS WORKS: VALUE(Yes) vs REFERENCE(No)
 
 const flight = 'LH234';
@@ -66,7 +65,7 @@ const newPassport = function(person) {
 
 newPassport(Anshu) ;
 checkIn(flight, Anshu);
-*/
+
 
 //--------------------------------------------------------------------------------------------------------------------------------//
 
@@ -99,3 +98,176 @@ const high5 = function() {
 document.body.addEventListener('click', high5);
 
 ['Riyanshu', 'Saloni', 'Aisha', 'Supriya'].forEach(high5);
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+// FUNCTION RETURNING FUNCTIONS
+
+const greet = function(greeting) {
+    return function(name) {
+        console.log(`${greeting} ${name}`);
+    };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Riyanshu');
+greeterHey('Anshu');
+
+greet('Hello')('Riyanshu');
+
+// CHALLENGE
+const greetArr = greeting => Name => console.log(`${greeting} ${Name}`);
+
+greetArr('Hi')('Riyanshu');
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+// THE CALL AND APPLY METHODS
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    // book: function() {}
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+
+        this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name});
+    },
+};
+
+lufthansa.book(239, 'Riyanshu Sharma');
+lufthansa.book(239, 'Anshu Sharma');
+console.log(lufthansa);
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Doesn't work
+// book(23, 'Aditya Sharma');
+
+book.call(eurowings, 23, 'Aditya Sharma');
+console.log(eurowings);
+
+book.call(lufthansa, 23, 'Indu Sharma');
+console.log(lufthansa);
+
+const swiss = {
+    airline: 'Swiss Air Lines',
+    iataCode: 'LX',
+    bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+// THE BIND METHOD
+
+// book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Anshu Sharma');
+bookEW23('Riyanshu Sharma');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+// lufthansa.buyPlane();
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+// CODING CHALLENGE #1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
