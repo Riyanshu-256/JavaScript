34ecb8b57df16c6981bf1d0fb2d65a92ef924c0c
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-04 15:04:53 +0530

    script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // DEFAULT PARAMETERS
 const bookings = [];
 
@@ -27,3 +28,29 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------//
+
+// HOW PASSING ARGUMENTS WORKS: VALUE vs REFERENCE
+
+const flight = 'LH234';
+const Anshu = {
+    name: 'Anshu Sharma',
+    passport: 1234567890,
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mrs.' + passenger.name;
+
+    if(passenger.passport === 1234567890) {
+        alert('Checked in');
+    }else{
+        alert('Wrong passport');
+    }
+};
+
+checkIn(flight, Anshu);
+console.log(flight);
+console.log(Anshu);
