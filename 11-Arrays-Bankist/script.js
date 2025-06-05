c7b15cc0186c951220b4f038c4b9a3ae2ddf49f6
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-05 17:13:16 +0530

    script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -224,7 +224,7 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 */
 
 //------------------------------------------------------------------------------------------------------//
-
+/*
 // THE MAP METHOD
 
 const eurToUsd = 1.1;
@@ -249,9 +249,10 @@ const movementsDescriptions = movements.map(
     )}`
 );
 console.log(movementsDescriptions);
+*/
 
 //------------------------------------------------------------------------------------------------//
-
+/*
 // THE FILTER METHOD
 
 const deposits = movements.filter(function (mov, i, arr) {
@@ -290,11 +291,12 @@ const max = movements.reduce((acc, mov) => {
   else return mov;
 }, movements[0]);
 console.log(max);
+*/
 
 //------------------------------------------------------------------------------------------------------------------------------//
 
 //-------------------------------------------CODING CHALLENGE #2---------------------------------------------------------------//
-
+/*
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);
@@ -315,5 +317,25 @@ const calcAverageHumanAge = function (ages) {
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+*/
 
 //--------------------------------------------------------------------------------------------------------------------------------//
+/*
+// THE MAGIC OF CHAINING METHOD
+const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+*/
+
+//------------------------------------------------------------------------------------------------------------------------------//
+
