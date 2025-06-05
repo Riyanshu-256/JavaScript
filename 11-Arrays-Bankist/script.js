f68a050758008dadaf3dd51b47f864359092237f
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-05 11:22:29 +0530

    script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -71,6 +71,62 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]; 
 
 /////////////////////////////////////////////////
+/*
+// SIMPLE ARRAY METHOD
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// slice method
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1)); // -1 --> last element
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// splice method
+//console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// reverse
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// join
+console.log([letters.join(' - ')]);
+*/
+//---------------------------------------------------------------------------------------------//
+/*
+// THE NEW AT METHOD
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length -1]);
+console.log(arr.slice(-1));
+console.log(arr.at(-1));
+
+// string
+console.log('Anshu'.at(0));
+console.log('Anshu'.at(-1));
+*/
+
+//----------------------------------------------------------------------------------------------//
+
+
