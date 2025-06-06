7f14101199d3eb31633f1cd58beddb711f105b99
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-06 23:01:15 +0530

    script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -126,8 +126,10 @@ logo.clasName = 'Anshu';
 */
 
 //------------------------------------------------------------------------------------------------------------------------------------------------//
+/*
 
 // IMPLEMENTING SMOOTH SCROLLING
+
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
@@ -159,5 +161,27 @@ btnScrollTo.addEventListener('click', function(e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+*/
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------//
+/*
+
+// TYPES OF EVENT AND EVENT HANDLERS
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
+*/
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
