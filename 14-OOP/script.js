06c3af0542ce0a8fa409dc3921642dd5a7af4b10
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-08 11:01:32 +0530

    script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// CONSTRUCTOR FUNCTION
+const Person = function(firstName, birthYear) {
+    console.log(this);
+}
+
+new Person('Anshu', 2005);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
