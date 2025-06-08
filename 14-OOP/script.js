848e11941955ce60e52a735ee40a9ea72e45c72f
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-08 11:19:51 +0530

    script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -2,9 +2,15 @@
 
 // CONSTRUCTOR FUNCTION
 const Person = function(firstName, birthYear) {
+    // Instances properties
     this.firstName = firstName;
     this.birthYear = birthYear;
-}
+
+    // Never to this
+    //this.calcAge = function(){
+    //   console.log(2025 - birthYear);
+    // };
+};
 
 const Anshu = new Person('Anshu', 2005);
 console.log(Anshu);
@@ -13,3 +19,11 @@ console.log(Anshu);
 // 2. function is called, this = {}
 // 3. {} linked to prototype
 // 4. function automatically return {}
+
+const Riyanshu = new Person('Riyanshu', 2005);
+const Riya = new Person('Riya', 2005);
+console.log(Riyanshu, Riya);
+
+console.log(Anshu instanceof Person);
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------//
