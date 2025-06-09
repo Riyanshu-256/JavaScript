1cbce5f4f963b82a052d96039ff25423f847c19a
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-09 16:38:03 +0530

    script.js

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -246,20 +246,7 @@ class App {
         </div>
     `;
 
-    if (workout.type === 'running')
-      html += `
-        <div class="workout__details">
-          <span class="workout__icon">⚡️</span>
-          <span class="workout__value">${workout.pace.toFixed(1)}</span>
-          <span class="workout__unit">min/km</span>
-        </div>
-        <div class="workout__details">
-          <span class="workout__icon">🦶🏼</span>
-          <span class="workout__value">${workout.cadence}</span>
-          <span class="workout__unit">spm</span>
-        </div>
-      </li>
-      `;
+    
 
     if (workout.type === 'cycling')
       html += `
