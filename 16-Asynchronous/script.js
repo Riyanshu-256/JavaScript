8a18273ef90dcf03dc99776edb3c3acc4a39b442
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-11 13:37:08 +0530

    CODING CHALLENGE #03

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -404,7 +404,7 @@ createImage('img/img-1.jpg')
   .then(() => {
     currentImg.style.display = 'none';
   })
-  .catch(err => console.error(err));
+  .catch(err => console.error(err)); 
 */
 
 //---------------------------------------------------------------------------------------------------------------//
