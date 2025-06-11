0742a0aa35daa8ef1be8f9642600e9ea178d0d41
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-11 12:26:27 +0530

    Coding Challenge #01

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -218,3 +218,31 @@ btn.addEventListener('click', function () {
 */
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------//
+
+//-------------------------------------------------------------------CODING CHALLENGE #O1-----------------------------------------------------------------//
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.countryCode}`);
+
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------//
