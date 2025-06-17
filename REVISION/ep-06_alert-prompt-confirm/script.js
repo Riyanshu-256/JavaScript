5de5c0f76560442af7e7adcc8fe33464eb59a7d4
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-18 00:55:55 +0530

    Alert-Prompt-Confirm

---
+++ REVISION/ep-06_alert-prompt-confirm/script.js
@@ -0,0 +1,8 @@
+// const alertResult = alert("Are you sure?")
+
+// const isConfirmed = confirm("Would you like to proceed?")
+
+const userInput = prompt("Please enter your name.........................")
+console.log(userInput)
+
+//------------------------------------------------------------COMPLETED-----------------------------------------------------//
