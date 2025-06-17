788cfd46a7c2eec83da34427488d99244d4323b8
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-17 23:50:04 +0530

    LET

--- REVISION/ep-04_javascript-variables/starter-code/scirpt.js
+++ REVISION/ep-04_javascript-variables/starter-code/scirpt.js
@@ -1,2 +1,23 @@
-const name1 = 'Riyanshu';
-console.log(name1);
+// LET(to change)
+let firstName = 'Riyanshu';
+let lastName = 'Sharma';
+let age = 22;
+let isHappy = true;
+
+firstName = 'Anshu';  // to change the firstName
+
+let userIntro = 'Hi I am' +' '+ firstName +' '+ lastName +'. I am' +' ' + age +' '+'years' ; // ' ' => to craete space b/w to words
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+// CONST(to remain the value same)
+const hoursInDay = 24;
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+// VAR(similar as LET)
+var Name = 'Riyanshu Sharma';
+var team = 'CODE BLOODED';
+var member = 9;
+
+var Intro = 'Hi Myself' +' '+ Name +'. Currently I am learning frontend course'+' '+'which is organised by' +' '+ team +' '+'member' ;
