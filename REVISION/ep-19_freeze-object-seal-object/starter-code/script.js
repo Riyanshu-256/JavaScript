91a78b30362953419b1599ae7c5982a40ee84a3d
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-18 22:13:02 +0530

    Seal Freeze

--- REVISION/ep-19_freeze-object-seal-object/starter-code/script.js
+++ REVISION/ep-19_freeze-object-seal-object/starter-code/script.js
@@ -1,15 +1,15 @@
 const user = {
-  firstName: 'Adarsh',
-  lastName: 'Singh',
-  pata: {
-    city: 'Bangalore',
-    pinCode: 876876,
-    state: 'Karnataka',
-    moreDetails: {
-      population: 9798897897,
-      area: '787 sq km',
-    },
-  },
-  age: 15,
-  isGraduate: false,
+    firstName: 'Anshu', lastName: 'Sharma', age: 20, education: 'JNV KODERMA', 
+     address: {
+        city: 'Koderma',
+        pinCode: 825109,
+        state: 'Jharkhand',
+        moreDetails: {
+            population: 9798897897,
+            area: '787 sq km',
+        }
+    }
 }
+
+Object.seal(user);
+//Object.freeze(user);
