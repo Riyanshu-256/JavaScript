9be445ce7da63ae59e2fe0db4cc4a986aae75db0
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-18 21:49:23 +0530

    Object

---
+++ REVISION/ep-18_reference-types-in-javascript/starter-code/script.js
@@ -0,0 +1,27 @@
+const myName = 'Riya';
+
+const userName1 = ' ';
+const userName2 = ' ';
+
+const user1 = {
+    firstName: 'Riyanshu', lastName: 'Sharma', age: 20, education: 'BTech', city: 'Koderma'}
+    console.log(user1);
+    console.log(user1.lastName);
+
+const user2 = {
+    firstName: 'Anshu', lastName: 'Sharma', age: 20, education: 'JNV KODERMA', 
+     address: {
+        city: 'Koderma',
+        pinCode: 825109,
+        state: 'Jharkhand',
+        moreDetails: {
+            population: 9798897897,
+            area: '787 sq km',
+        }
+    }
+}
+
+
+    console.log(user2);
+    console.log(user2.lastName);
+    console.log(user2["lastName"]);
