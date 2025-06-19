77ae8d4dc5aaf81da93362d17f7078ed0dc692da
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-19 18:58:59 +0530

    Function

---
+++ REVISION/ep-28_introduction-to-functions/starter-code/script.js
@@ -0,0 +1,29 @@
+console.log('Hi,');
+console.log('My name is Anshu Sharma');
+console.log('I am a web developer.');
+console.log('*********************************');
+
+console.log('Hi,');
+console.log('My name is Anshu Sharma');
+console.log('I am a web developer.');
+console.log('*********************************');
+
+console.log('Hi,');
+console.log('My name is Anshu Sharma');
+console.log('I am a web developer.');
+console.log('*********************************');
+
+//-------------------------------------------------------------------------------------------------------------//
+
+// defination
+function introduceMe(username, profession, age) {      // return value of function is undefined
+    console.log('Hi,');
+    console.log(`My name is ${username}`);
+    console.log(`I am a ${profession}`);
+    console.log(`I am ${age} years old`)
+}
+
+// call->introduceMe
+// const returnValue = introduceMe()
+introduceMe('Riyanshu', 'Software Engineer', 20);
+introduceMe('Anshu', 'Mechanical Engineer', 22);
