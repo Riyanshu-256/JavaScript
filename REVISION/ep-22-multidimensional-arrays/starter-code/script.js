45db1e2ade248f3bb72a570bbb4765738428181d
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-19 16:17:31 +0530

    Multidimensional array

---
+++ REVISION/ep-22-multidimensional-arrays/starter-code/script.js
@@ -0,0 +1,11 @@
+const nameAndNumberList = [
+  ['Riyanshu', 99],
+  ['Anshu', 98],
+  ['Riya', 97],
+]
+
+const ticTacToe = [
+    ['X', null, null],
+    [null, null, 'O'],
+    ['O', null, 'X']
+]
