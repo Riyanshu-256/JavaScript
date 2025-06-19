716af03e324bf220fe0af583b746d78742dd8d3d
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-19 16:24:14 +0530

    Compound Operator

---
+++ REVISION/ep-24-compound-operators/starter-code/script.js
@@ -0,0 +1,33 @@
+const username = 'Riyanshu'
+
+let num = 10
+
+// debugger
+// num = num + 10
+// num = num + 10
+// num = num + 10
+// num = num + 10
+
+// num += 10
+// num += 10
+// num += 10
+// num += 10
+
+// num = num * 2
+// num = num * 2
+// num = num * 2
+
+// num *= 2
+// num *= 2
+// num *= 2
+
+// const newNum = ++num
+// const newNum = --num
+
+// const newNum = num++
+// const newNum = num--
+
+// num = num + 1
+// num += 1
+
+num++
