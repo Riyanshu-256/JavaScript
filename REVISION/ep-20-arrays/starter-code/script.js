9372175a06614cb879920ce9eaa7ab7cee22383e
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-19 10:56:00 +0530

    Arrays

--- REVISION/ep-20-arrays/starter-code/script.js
+++ REVISION/ep-20-arrays/starter-code/script.js
@@ -1,6 +1,15 @@
-const username = 'Anurag Singh'
+const username = 'Riyanshu Sharma'
 
-const fruitsCollection = ['Apple', 'Banana', 'Grapes', 'Dates']
+// const fruitsCollection = ['Apple', 'Banana', 'Grapes', 'Dates', ['Anshu', 'Riyanshu', 'Riya']];
+
+const colors = ['Violet', 'Indigo', 'Blue', 'Green', 'Yellow', 'Orange', 'Red'];
+
+// to add element
+colors.push('Black', 'Pink', 'Skyblue');
+
+// to remove element
+colors.pop();
+
 
 const newObject = {}
 const newArray = []
