22018e196826d822f490f7a80a93077a59cfbe20
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-20 11:13:08 +0530

    FOR OF & FOR IN

--- REVISION/ep-41_for-of-vs-for-in-loop/starter-code/script.js
+++ REVISION/ep-41_for-of-vs-for-in-loop/starter-code/script.js
@@ -1,2 +1,39 @@
 const fruits = ['banana', 'apple', 'peach', 'mango', 'grapes']
 
+for(let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+console.log('*********************************');
+
+for(const fruit of fruits) {
+    console.log(fruit);
+}
+
+const user = 'Anurag Singh'
+
+
+for(const letter of user) {
+    console.log(letter);
+}
+
+const person = {
+    firstName: 'Riyanshu',
+    lastName: 'Sharma',
+    age: 20,
+    eyeColor: 'black',
+    city: 'Koderma',
+}
+
+// for(const key in person) {
+//     console.log(key, ': ' ,person[key]);
+// }
+
+const personKeys = Object.keys(person)
+const personValues = Object.values(person)
+const personEntries = Object.entries(person)
+// for (const key of personKeys) {
+//     console.log(person[key]);
+// }
+
+
