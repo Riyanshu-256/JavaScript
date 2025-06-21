c141a9518ad2d23a23cacb3db8b971a50cfd0dc1
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-21 07:28:20 +0530

    Default Parameter

--- REVISION/ep-46_default-params/starter-code/script.js
+++ REVISION/ep-46_default-params/starter-code/script.js
@@ -2,3 +2,10 @@ function multiply(a, b) {
   return a * b
 }
 
+function multiply(a, b = 1) {
+  return a * b
+}
+
+function rollADie(numberOfSides = 6) {
+  return Math.floor(Math.random() * numberOfSides) + 1
+}
